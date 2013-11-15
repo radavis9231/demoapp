2779b993e474c381f23da0e02b47ae8dd62de1c5
radavis9231 <drew_davis@mba.berkeley.edu>
2013-11-15 03:45:16 +0000

    New app with users from Haas

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demoapp::Application.config.secret_key_base = '7b06fc5eb9dab56148988bd027eec1cdae83fd64d9e246115aa85acbc6711ab3cea6d34ca7b3168bdf19b71de5ed6371b10ef277bcfde9eaad36d2893ba11a05'
