4eef1159532d8dc2e37a721a8a5983e799fcee1c
radavis9231 <drew_davis@mba.berkeley.edu>
2013-11-22 03:10:26 +0000

    Made new static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Demoapp::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+  get "staic_pages/home"
+  get "staic_pages/help"
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
