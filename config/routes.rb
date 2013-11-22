3c6caf127681237bcc7a81076c9c1c8a8781fa46
radavis9231 <drew_davis@mba.berkeley.edu>
2013-11-22 03:16:22 +0000

    Changed home page path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Demoapp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
+  root 'static_pages#home'
+  match '/help', to: 'static_pages#help', via: 'get'
   get "staic_pages/home"
   get "staic_pages/help"
   resources :users
